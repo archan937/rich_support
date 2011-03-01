3bcaf756973a254ab744b007cc4197fc32da9f45
Paul Engel <paul.engel@holder.nl>
2011-03-01 01:40:40 +0100

    Introduced Rich::Support.after_initialize which calls the Rails after_initialize method correctly (whether it is a Rails 2 or 3 app)

--- lib/rich/support.rb
+++ lib/rich/support.rb
@@ -5,6 +5,13 @@ module Rich
     def append_gem_path(path)
       (@gem_paths ||= []) << path
       check_routes_for(path)
+
+    def after_initialize(&block)
+      if Rails::VERSION::MAJOR >= 3
+        config.after_initialize &block
+      else
+        Rails.configuration.after_initialize &block
+      end
     end
 
   private
