c50bb38fa2e24178bb6ff68f51e5e4c3a1c0ede3
Paul Engel <paul.engel@holder.nl>
2011-03-01 01:41:24 +0100

    Adding the controllers and views path and routes.rb when required

--- lib/rich/support.rb
+++ lib/rich/support.rb
@@ -4,7 +4,11 @@ module Rich
 
     def append_gem_path(path)
       (@gem_paths ||= []) << path
-      check_routes_for(path)
+
+      check_controllers_for path
+      check_routes_for      path
+      check_views_for       path
+    end
 
     def after_initialize(&block)
       if Rails::VERSION::MAJOR >= 3
@@ -16,8 +20,31 @@ module Rich
 
   private
 
-    def check_routes_for
-      require File.expand_path("config/routes", path) if Rails::VERSION::MAJOR < 3
+    def check_controllers_for(path)
+      return if Rails::VERSION::MAJOR >= 3
+      if File.exists?(controllers = File.expand_path("app/controllers", path))
+        $LOAD_PATH << controllers
+        ActiveSupport::Dependencies.autoload_paths << controllers
+        ActiveSupport::Dependencies.autoload_once_paths.delete controllers
+      end
+    end
+
+    def check_routes_for(path)
+      return if Rails::VERSION::MAJOR >= 3
+      if File.exists?(routes = File.expand_path("config/routes.rb", path))
+        require routes
+      end
+    end
+
+    def check_views_for(path)
+      return if Rails::VERSION::MAJOR >= 3
+      if File.exists?(views = File.expand_path("app/views", path))
+        if ActionController::Base.respond_to? :append_view_path
+          ActionController::Base.append_view_path views
+        elsif ActionController::Base.respond_to? :view_paths
+          ActionController::Base.self.view_paths << views
+        end
+      end
     end
 
   end
