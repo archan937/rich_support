58fbaaa2dc75ef449914adcca16a8cfe76868f0e
Paul Engel <paul.engel@holder.nl>
2011-04-09 19:57:19 +0200

    Alias methoding String.copy_case to String.cp_case (also for String.copy_case!)

--- lib/rich/support/core/string/inflections.rb
+++ lib/rich/support/core/string/inflections.rb
@@ -10,17 +10,19 @@ module Rich
               self[0].chr.capitalize + self[1, size]
           end
 
-          def cp_case(s)
+          def copy_case(s)
             send((:downcase unless s.dup.downcase!) || (:upcase unless s.dup.upcase!) || (:upcase_first unless s.dup.upcase_first!) || :to_s)
           end
+          alias_method :cp_case, :copy_case
 
           def upcase_first!
             self == (result = upcase_first) ? nil : replace(result)
           end
 
-          def cp_case!(s)
+          def copy_case!(s)
             self == (result = cp_case(s))   ? nil : replace(result)
           end
+          alias_method :cp_case!, :copy_case!
 
           def singularize!
             downcase == (result = singularize).downcase ? nil : replace(result)
