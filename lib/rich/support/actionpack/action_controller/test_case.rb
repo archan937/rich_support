ccd6edac4a5341f03122a87ec98191772b7c564e
Paul Engel <paul.engel@holder.nl>
2011-05-15 02:15:00 +0200

    Using Hash.key when running Ruby 1.9.2 (which solves deprecation warning)

--- lib/rich/support/actionpack/action_controller/test_case.rb
+++ lib/rich/support/actionpack/action_controller/test_case.rb
@@ -14,8 +14,9 @@ module ActionController
             [route.name, route.path.gsub("(.:format)", "")] unless route.name.blank?
           end
         else
+          method = ActionController::Routing::Routes.named_routes.routes.respond_to?(:key) ? :key : :index
           ActionController::Routing::Routes.routes.collect do |route|
-            name = ActionController::Routing::Routes.named_routes.routes.index(route).to_s
+            name = ActionController::Routing::Routes.named_routes.routes.send(method, route).to_s
             path = route.segments.collect(&:to_s).join
             path.chop! if path.length > 1
             [name, path] unless name.blank?
